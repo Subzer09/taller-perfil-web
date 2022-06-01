407a251bbb545a237f55de73d7f830439eb9b902
Subzer09 <jimmypaytan10@gmail.com>
2022-05-31 23:58:59 -0500

    mi primera web

---
+++ index.php
@@ -0,0 +1,446 @@
+<!DOCTYPE html>
+<html lang="es"> 
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<meta name="description" content="Deebo">
+<meta name="author" content="Frenify">
+<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+
+<title>Mi Nombre</title>
+
+<link rel="preconnect" href="https://fonts.googleapis.com" />
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+<link href="https://fonts.googleapis.com/css2?family=Jost:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet" />
+
+<link type="text/css" rel="stylesheet" href="css/base.css?ver=4" />
+<link type="text/css" rel="stylesheet" href="css/magnific.css?ver=4" />
+<link type="text/css" rel="stylesheet" href="css/animated-headlines.css?ver=4" />
+<link type="text/css" rel="stylesheet" href="css/style.css?ver=4" />
+
+</head>
+
+<body>
+
+<div class="deebo_fn_main">
+			
+			
+	<!-- Claro/Oscuro Cambiar Colores -->
+	<div class="deebo_fn_switcher_wrap">
+		<label>
+			<span class="light">Claro</span>
+				<span class="checkbox_wrap">
+					<input type="checkbox" id="skin_switcher" checked>
+					<span class="slider round"></span>
+				</span>
+			<span class="dark">Oscuro</span>
+		</label>
+	</div>
+	<!-- /Light/Dark Cambiar Colores -->
+	
+	
+	<!-- Overlay -->
+	<div class="right_bar_overlay"></div>
+	<!--/ Overlay -->
+
+	<!-- MODALBOX -->
+	<div class="deebo_fn_modalbox">
+		<a class="extra_closer" href="#"></a>
+		<div class="box_inner">
+			<a class="closer" href="#"><span></span></a>
+			<div class="modal_content">
+
+				<div class="modal_in">
+					<!-- Content comes from JS -->
+				</div>
+
+				<div class="fn__nav" data-from="" data-index="">
+					<a href="#" class="prev">
+						<span class="text">Anterior</span>
+						<span class="arrow_wrapper"><span class="arrow"></span></span>
+					</a>
+					<a href="#" class="next">
+						<span class="text">Siguiente</span>
+						<span class="arrow_wrapper"><span class="arrow"></span></span>
+					</a>
+				</div>
+			</div>
+		</div>
+	</div>
+	<!-- /MODALBOX --> 
+
+	<!-- Overlay -->
+	<div class="right_bar_overlay"></div>
+	<!--/ Overlay -->
+
+	<!-- MODALBOX -->
+	<div class="deebo_fn_modalbox">
+		<a class="extra_closer" href="#"></a>
+		<div class="box_inner">
+			<a class="closer" href="#"><span></span></a>
+			<div class="modal_content">
+
+				<div class="modal_in">
+					<!-- Content comes from JS -->
+				</div>
+
+				<div class="fn__nav" data-from="" data-index="">
+					<a href="#" class="prev">
+						<span class="text">Anterior</span>
+						<span class="arrow_wrapper"><span class="arrow"></span></span>
+					</a>
+					<a href="#" class="next">
+						<span class="text">Siguiente</span>
+						<span class="arrow_wrapper"><span class="arrow"></span></span>
+					</a>
+				</div>
+			</div>
+		</div>
+	</div>
+	<!-- /MODALBOX --> 
+
+	<!-- Modal CV Card -->
+	<div class="deebo_fn_cv">
+
+		<!-- CV Inner -->
+		<div class="deebo_fn__cv">
+
+			<!-- CV Background -->
+			<div class="cv__bg"></div>
+			<div class="cv__bg2"></div>
+			<!-- /CV Background -->
+
+			<!-- CV Left Side -->
+			<div class="cv__header">
+				<div class="in">
+					<div class="avatar">
+						<img src="img/header/1.jpg" alt="">
+					</div>
+					<h3><span>Jimmy</span> PAYTAN</h3>
+					<p class="quote">I'm a passionate front-end web developer who excels at creating pixel-perfect web interfaces. I’m available for a freelance job.</p>
+					<ul class="contact_info">
+						<li>
+							<span class="icon"><img src="svg/location.svg" alt="" class="fn__svg"></span>
+							<p>Av. Grau 1725</p>
+						</li>
+						<li>
+							<span class="icon"><img src="svg/call.svg" alt="" class="fn__svg"></span>
+							<p>(+706) 898-0751</p>
+						</li>
+						<li>
+							<span class="icon"><img src="svg/message.svg" alt="" class="fn__svg"></span>
+							<p>adam_milner@example.com</p>
+						</li>
+					</ul>
+					<ul class="social">
+						<li>
+							<a href="#" target="_blank" title="twitter.com">
+								<img class="fn__svg" src="svg/twitter.svg" alt="twitter.com" />
+							</a>
+						</li>
+						<li>
+							<a href="#" target="_blank" title="facebook.com">
+								<img class="fn__svg" src="svg/facebook-logo.svg" alt="facebook.com" />
+							</a>
+						</li>
+						<li>
+							<a href="#" target="_blank" title="instagram.com">
+								<img class="fn__svg" src="svg/instagram.svg" alt="instagram.com" />
+							</a>
+						</li>
+						<li>
+							<a href="#" target="_blank" title="printerest.com">
+								<img class="fn__svg" src="svg/pinterest.svg" alt="printerest.com" />
+							</a>
+						</li>
+						<li>
+							<a href="#" target="_blank" title="youtube.com">
+								<img class="fn__svg" src="svg/youtube.svg" alt="youtube.com" />
+							</a>
+						</li>
+					</ul>
+				</div>
+			</div>
+			<!-- /CV Left Side -->
+
+			<!-- /CV Contenido Side -->
+			<div class="cv__content">
+				
+				<section id="hero-header" class="section_header">
+					<div class="content">
+						<div class="left_hero_header">
+							<div class="circle">
+								<div class="bg_img" data-bg-img="img/header/1.jpg"></div>
+								<img src="img/thumb/square.jpg" alt="" />
+								<div class="circle_holder_blue"><span></span></div>
+								<div class="circle_holder_orange"><span></span></div>
+								<div class="lines">
+									<span></span>
+									<span></span>
+									<span></span>
+								</div>
+							</div>
+						</div>
+						<div class="right_hero_header">
+							<div class="my_self">
+								<h4>Hello! I'm a</h4>
+								<h2>
+									<span class="cd-headline clip">
+										<span class="cd-words-wrapper">
+										  <b class="is-visible">Designer</b>
+										  <b>Developer</b>
+										  <b>Freelancer</b>
+										</span>
+									</span>
+								</h2>
+							</div>
+						</div>
+					</div>
+				</section>
+
+
+				<!-- CV: Sección Biografía -->
+				<section id="cv_biography">
+					<div class="section_title">
+						<h3>Biografía</h3>
+					</div>
+					<p>I'm a Freelancer Front-end Developer with over 12 years of experience. I'm from San Francisco. I code and create web elements for amazing people around the world. I like work with new people. New people are new experiences.</p>
+					<div class="fn_cs_info_items">
+						<ul>
+							<li><p>Nombres: <span>Adam Milner</span></p></li>
+							<li><p>Apellidos: <span>Adam Milner</span></p></li>
+							<li><p>Cumpleaños: <span>June 14, 1992</span></p></li>
+							<li><p>Edad: <span>29 Years</span></p></li>
+							<li><p>Nacionalidad: <span>American</span></p></li>
+							<li><p>Email: <span>London, UK</span></p></li>
+							<li><p>Teléfono: <span><a href="tel:+17785355344">(+1) 7785 355 344</a></span></p></li>
+						</ul>
+					</div>
+				</section>
+				<!-- /CV: Sección Biografía -->
+
+
+				<!-- CV: Sección Educación -->
+				<section id="cv_education">
+					<div class="section_title">
+						<h3>Educación</h3>
+					</div>
+					<div class="fn_cs_boxed_list">
+						<ul>
+							<li>
+								<div class="item">
+									<div class="item_top">
+										<h5>Frenify University</h5>
+										<span>( 2014 — 2017 )</span>
+									</div>
+									<h3>Computer Science</h3>
+									<p>Adipisicing Lorem ipsum dolor sit amet, consectetur elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </p>
+								</div>
+							</li>
+							<li>
+								<div class="item">
+									<div class="item_top">
+										<h5>Edu University</h5>
+										<span>( 2011 — 2014 )</span>
+									</div>
+									<h3>Master Degree</h3>
+									<p>Adipisicing Lorem ipsum dolor sit amet, consectetur elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </p>
+								</div>
+							</li>
+							<li>
+								<div class="item">
+									<div class="item_top">
+										<h5>Clolumbia College</h5>
+										<span>( 2007 — 2011 )</span>
+									</div>
+									<h3>Bachelor Degree</h3>
+									<p>Adipisicing Lorem ipsum dolor sit amet, consectetur elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </p>
+								</div>
+							</li>
+						</ul>
+					</div>
+				</section>
+				<!-- /CV: Sección Educación -->
+
+
+				<!-- CV: Sección Experiencia Laboral -->
+				<section id="cv_experience">
+					<div class="section_title">
+						<h3>Experiencia Laboral</h3>
+					</div>
+					<div class="fn_cs_boxed_list">
+						<ul>
+							<li>
+								<div class="item">
+									<div class="item_top">
+										<h5>Frenify  LLC</h5>
+										<span>( 2018 — Today )</span>
+									</div>
+									<h3>Sr. Front-end Engineer</h3>
+									<p>Adipisicing Lorem ipsum dolor sit amet, consectetur elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </p>
+								</div>
+							</li>
+							<li>
+								<div class="item">
+									<div class="item_top">
+										<h5>Google  LLC</h5>
+										<span>( 2016 — 2018 )</span>
+									</div>
+									<h3>Front-end Developer</h3>
+									<p>Adipisicing Lorem ipsum dolor sit amet, consectetur elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </p>
+								</div>
+							</li>
+							<li>
+								<div class="item">
+									<div class="item_top">
+										<h5>Twitter  LLC</h5>
+										<span>( 2016 — 2011 )</span>
+									</div>
+									<h3>Graphic Designer</h3>
+									<p>Adipisicing Lorem ipsum dolor sit amet, consectetur elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </p>
+								</div>
+							</li>
+						</ul>
+					</div>
+				</section>
+				<!-- /CV: Sección Experiencia Laboral -->
+
+
+				<!-- CV: Sección Portfolio-->
+				<section id="cv_portfolio">
+					<div class="section_title">
+						<h3>Portfolio</h3>
+					</div>
+					
+					<!-- Portfolio Shortcode -->
+					<div class="fn_cs_portfolio">
+					
+						<!-- Portfolio Filter -->
+						<div class="portfolio_filter">
+							<ul>
+								<li><a href="#" class="current" data-filter="*">All</a></li>
+								<li><a href="#" data-filter=".youtube">Youtube</a></li>
+								<li><a href="#" data-filter=".vimeo">Vimeo</a></li>
+								<li><a href="#" data-filter=".soundcloud">Soundcloud</a></li>
+								<li><a href="#" data-filter=".popup">Popup</a></li>
+							</ul>
+						</div>
+						<!-- /Portfolio Filter -->
+						
+						
+						<!-- Portfolio List -->
+						<div class="portfolio_list">
+							<ul class="gallery_zoom grid">
+								<li class="youtube grid-item">
+									<div class="inner">
+										<div class="entry">
+											<a class="popup-youtube" href="https://www.youtube.com/watch?v=7e90gBu4pas">
+												<img src="img/thumb/42-56.jpg" alt="" />
+												<div class="main" data-bg-img="img/portfolio/1.jpg"></div>
+												<div class="mobile_title">
+													<h3>Mockup Shape</h3>
+													<span>Youtube</span>
+												</div>
+											</a>
+										</div>
+									</div>
+								</li>
+								<li class="vimeo grid-item">
+									<div class="inner">
+										<div class="entry">
+											<a class="popup-vimeo" href="https://vimeo.com/337293658">
+												<img src="img/thumb/42-34.jpg" alt="" />
+												<div class="main" data-bg-img="img/portfolio/2.jpg"></div>
+												<div class="mobile_title">
+													<h3>Ave Bottle</h3>
+													<span>Vimeo</span>
+												</div>
+											</a>
+										</div>
+									</div>
+								</li>
+								<li class="soundcloud grid-item">
+									<div class="inner">
+										<div class="entry">
+											<a class="soundcloude_link mfp-iframe audio" href="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/471954807&color=%23ff5500&auto_play=true&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true">
+												<img src="img/thumb/42-56.jpg" alt="" />
+												<div class="main" data-bg-img="img/portfolio/3.jpg"></div>
+												<div class="mobile_title">
+													<h3>Magic Art</h3>
+													<span>Soundcloud</span>
+												</div>
+											</a>
+										</div>
+									</div>
+								</li>
+								<li class="popup grid-item">
+									<div class="inner">
+										<div class="entry">
+											<a class="zoom" href="img/portfolio/5.jpg">
+												<img src="img/thumb/42-56.jpg" alt="" />
+												<div class="main" data-bg-img="img/portfolio/5.jpg"></div>
+												<div class="mobile_title">
+													<h3>Blue Lemon</h3>
+													<span>Popup</span>
+												</div>
+											</a>
+										</div>
+									</div>
+								</li>
+								<li class="popup grid-item">
+									<div class="inner">
+										<div class="entry">
+											<a class="zoom" href="img/portfolio/4.jpg">
+												<img src="img/thumb/42-34.jpg" alt="" />
+												<div class="main" data-bg-img="img/portfolio/4.jpg"></div>
+												<div class="mobile_title">
+													<h3>Art Stone</h3>
+													<span>Popup</span>
+												</div>
+											</a>
+										</div>
+									</div>
+								</li>
+								<li class="popup grid-item">
+									<div class="inner">
+										<div class="entry">
+											<a class="zoom" href="img/portfolio/6.jpg">
+												<img src="img/thumb/42-34.jpg" alt="" />
+												<div class="main" data-bg-img="img/portfolio/6.jpg"></div>
+												<div class="mobile_title">
+													<h3>Global Evolution</h3>
+													<span>Popup</span>
+												</div>
+											</a>
+										</div>
+									</div>
+								</li>
+							</ul>
+						</div>
+						<!-- /Portfolio List -->
+						
+					</div>
+					<!-- /Portfolio Shortcode -->
+					
+				</section>
+				<!-- /CV: Sección Portfolio-->
+	
+			</div>
+			<!-- /CV Contenido Side -->
+		</div>
+		<!-- CV Inner -->
+	</div>
+	<!-- /Modal CV Card -->
+
+</div>
+
+<!-- Scripts -->
+<script type="text/javascript" src="js/jquery.js?ver=4"></script>
+<script type="text/javascript" src="js/isotope.js"></script>
+<script type="text/javascript" src="js/magnific.js"></script>
+<script type="text/javascript" src="js/animated-headlines.js?ver=4"></script>
+<script type="text/javascript" src="js/waypoints.js?ver=4"></script>
+<script type="text/javascript" src="js/init.js?ver=4"></script>
+<!-- /Scripts -->
+
+</body>
+</html>
